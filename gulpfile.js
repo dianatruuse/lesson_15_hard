7be488f8486ec98ebfccff27a63d537e93a16f5f
Diana Truuse <diana.truuse98@gmail.com>
2020-03-04 12:53:32 +0200

    feat: add gulpfile.js and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+  const {src, dest, watch} = require('gulp');
+  const browserSync = require('browser-sync').create();
+  const sass = require('gulp-sass')
+
+
+  // Static server
+  function bs() {
+    servseSass();
+    browserSync.init({
+      server: {
+        baseDir: "./"
+      }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", servseSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+  };
+
+  // Compile sass into CSS & auto-inject into browsers
+  function servseSass() {
+    return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+  };
+
+  exports.serve = bs;
